7ca252b1fad75918f929a765a92d800656c34388
Roni Akiola <roni.akiola@hotmail.com>
2023-06-04 20:22:07 +0300

    item class added

---
+++ Item.cs
@@ -0,0 +1,42 @@
+public class Item
+{
+  string _barcode;
+  string _name;
+  int _quantity;
+
+  public Item(string _barcode, string _name, int _quantity)
+  {
+    this._barcode = _barcode;
+    this._name = _name;
+    this._quantity = _quantity;
+  }
+
+  public void IncreaseQuantity(int amount)
+  {
+    if (amount > 0)
+    {
+      _quantity += amount;
+    }
+  }
+  public void DecreaseQuantity(int amount)
+  {
+    if (amount > 0 && _quantity > amount)
+    {
+      _quantity -= amount;
+    }
+  }
+  public string Barcode
+  {
+    get { return _barcode; }
+  }
+
+  public string Name
+  {
+    get { return _name; }
+  }
+
+  public int Quantity
+  {
+    get { return _quantity; }
+  }
+}
